fd5f9e96f2e328b6841b746994fbc0b6b9b8cca0
jose sanchez <sanchez24jose@gmail.com>
2019-01-24 21:24:27 -0600

    added hangman figure and updated html

---
+++ scripts/scripts.js
@@ -0,0 +1,60 @@
+let c = document.getElementById("myCanvas");
+let ctx = c.getContext("2d");
+let answer = "house";
+let submit = document.querySelector('.btn');
+
+
+
+
+function createBase() {
+	ctx.beginPath();
+	ctx.moveTo(100, 600);
+	ctx.lineTo(100, 100);
+	ctx.lineTo(300, 100);
+	ctx.lineTo(300, 175);
+	ctx.lineWidth = 10;
+	ctx.strokeStyle = "black";
+	ctx.stroke();
+}
+createBase();
+
+submit.addEventListener('click', function(){
+ let guess = document.querySelector('.form-control').value; 
+  console.log("guess:", guess);
+  
+  
+});
+
+
+
+
+
+//circle
+ctx.beginPath();
+ctx.arc(295, 220, 50, 0, 2 * Math.PI);
+ctx.stroke();
+
+// straight line for the body 
+ctx.beginPath();
+ctx.moveTo(300, 500);
+ctx.lineTo(300, 270);
+//right arm
+ctx.moveTo(300, 350);
+ctx.lineTo(400, 300);
+//left arm
+ctx.moveTo(300, 350);
+ctx.lineTo(200, 300);
+//right leg
+ctx.moveTo(369, 525);
+ctx.lineTo(300, 495);
+//left leg
+ctx.moveTo(230, 520);
+ctx.lineTo(300, 495);
+
+ctx.stroke();
+
+
+
+
+
+
